0ae63320e03d3a2214b9216f6a6c1b822a165d46
Dominique Feyer <dfeyer@ttree.ch>
2016-08-28 15:23:44 +0200

    TASK: Include command handler monitoring aspect

---
+++ Classes/Command/Aspect/CommandHandlerMonitoringAspect.php
@@ -0,0 +1,54 @@
+<?php
+namespace Ttree\Cqrs\MonitoringHelper\Command\Aspect;
+
+/*
+ * This file is part of the Ttree.Cqrs.MonitoringHelper package.
+ *
+ * (c) Hand crafted with love in each details by medialib.tv
+ */
+
+use Ttree\Cqrs\Event\EventType;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Aop\JoinPointInterface;
+use TYPO3\Flow\Log\SystemLoggerInterface;
+
+/**
+ * @Flow\Scope("singleton")
+ * @Flow\Aspect
+ */
+class CommandHandlerMonitoringAspect
+{
+    /**
+     * @var SystemLoggerInterface
+     * @Flow\Inject
+     */
+    protected $logger;
+
+    /**
+     * @var boolean
+     * @Flow\InjectConfiguration(path="command.monitoring.commandHandlerMonitoring.enabled")
+     */
+    protected $enabled;
+
+    /**
+     * @Flow\Around("within(Ttree\Cqrs\Command\CommandHandlerInterface) && method(public .*->handle())")
+     * @param JoinPointInterface $joinPoint
+     */
+    public function log(JoinPointInterface $joinPoint)
+    {
+        if ($this->enabled) {
+            $startTime = microtime(true);
+
+            $joinPoint->getAdviceChain()->proceed($joinPoint);
+
+            $command = $joinPoint->getMethodArgument('command');
+
+            $this->logger->log(vsprintf('action=monitoring type=command-handler command="%s" elapsed_time=%f', [
+                EventType::create($command),
+                microtime(true) - $startTime
+            ]), LOG_DEBUG);
+        } else {
+            $joinPoint->getAdviceChain()->proceed($joinPoint);
+        }
+    }
+}
